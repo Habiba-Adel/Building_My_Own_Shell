ae695490d1eef1248d08fcd4ea9fccc30f4a9956
habiba abdelgowad <habibaabdelgowad9@gmail.com>
2025-09-10 06:09:58 +0200

    Finishing step 4, 5 and test all the steps until now

--- app/main.go
+++ app/main.go
@@ -3,60 +3,142 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 	"strings"
+  
 )
+
+//i will seperate the handling the built in commands in alone function cause both can use it and rather of duplication i will do that
+//i pass the io.writer object to enable both functions call it without repeating 
+func HandlingBuiltInCommands (command string, here io.Writer) bool{
+  Args := strings.Split(command, " ")
+
+	if Args[0] == "exit" {
+    os.Exit(0)
+		return true
+	}
+
+	//now lets start implementing the second built in command which is the cd
+	if Args[0] == "cd" {
+		//there is a function in os that chaning the current directory
+		err := os.Chdir(Args[1])
+		if err != nil {
+			fmt.Fprintln(here,"This directory is not existed ^^ ")
+		}
+		return true
+	}
+
+	//now lets handle the environment variables
+	if Args[0] == "export" {
+		//we need here to read the name of the variable and the value and then passing it to save them in the environmet values
+		//and there is just export command it just appear to us all the defines variables in this environment
+
+		//we have 2 options here the first option if the user just write export that means he want to see all the defines variables
+		if len(Args) == 1 {
+			all := os.Environ()
+			//there is a for each in go and that by using the range word
+			for _, item := range all {
+				fmt.Fprintln(here,item)
+			}
+		} else {
+			//so that means the user enter name and value and we will need to split the second argument based on 0 and then send it to os to save it
+			info := strings.Split(Args[1], "=")
+			//now we will need to send the first and the second to the os to save them
+			if len(info) != 2 { //that means the user entered invlaid things and that will lead to exception
+				fmt.Println("Invalid formation for export command ^^ ")
+				return true
+			}
+			os.Setenv(info[0], info[1])
+
+		}
+		return true
+	}
+	//now the second thing is when the user enter echo command so we will need to print to it the value of this key
+	if Args[0] == "echo" {
+		//there is 2 things here if the first character in the key name sis $ so we will print to it the val else so we will print to it the key again
+		//if just echo word we will print empty line like real shells
+		if len(Args) == 1 {
+			fmt.Fprintln(here)
+			return true
+		}
+		if Args[1][0] == '$' {
+			fmt.Fprintln(here,os.Getenv(Args[1][1:]))
+		} else {
+			//we will just print the same key again
+			fmt.Fprintln(here,Args[1])
+		}
+		return true
+
+	}
+	//now the third thing is to implement the unset and it will delete the mentioned key from the variables
+	if Args[0] == "unset" { //its all cases in the reall shell it going us to the prompt again
+		os.Unsetenv(Args[1])
+		return true 
+	}
+  //otherwise is there is no any valid condtion for it that means it is an external one so
+  return false
+
+}
+
+
+
+// first the pipe commands handling function
+func HandlingPipe(line string) {
+	//now we have the whole entered line without any whote spcae
+	//now our first step is to splite it in array of strings based on |
+// 	commands := strings.Split(line, "|")
+// 	//and now each command from it will need to define its stdin and its std out and then split its command things to can pass it
+// //first thing we need to define the pbject that will store the inital dat to it 
+// previoushehe:=io.Reader(nil)
+//   //we have 3 special cases here the firs tone cause it will not having previous inptu and the last one cause it will write in the os and the normal cases 
+// 	for index, command := range commands {
+
+
+// }
+}
+
+// second is the non pipe commands handling function
+func HandlingNonPipe(line string) {
+	//we will pass it first cause if is it built in so handle it and return 
+  //cause it here in the non pipe version our result must appear directly in the terminal so that why we pass the os
+  if HandlingBuiltInCommands(line,os.Stdout) == true {//that means it is a built in one so just return 
+   return 
+  }
+
+	//using the exec is better very much rather than using the start process one 
+  //now the second option is that the command is external one
+  Args := strings.Split(line, " ")
+	cmd := exec.Command(Args[0], Args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("This command is not existed "+"😝 ")
+	}
+}
 
 func main() {
 
 	//lets implement REPL read eval print loop
 	//it will be infinit loop
 	for {
-		//this is called prompot of the shell
-		fmt.Print("<Habiba_Shell>$ ") //fprint here used to print what you write but in another extra thing like a file or something
-		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		command = strings.TrimSpace(command) //this is for removing the white spaces and inline characters from the command
+		//this is called prompot of the shell  and with the current working directory
+		cwd, _ := os.Getwd()
+    fmt.Print("🚀\033[1;31m<Habiba_Shell>\033[0m:" + cwd + " \033[1;31m$\033[0m ")//fprint here used to print what you write but in another extra thing like a file or something
+		line, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		line = strings.TrimSpace(line) //this is for removing the white spaces and inline characters from the command
 
-//now we want to split the command to can get its arguments 
-Args:=strings.Split(command," ")
-
-
-
-		if command == "exit" {
-			break
+		//now the first thing we will need to know if the entered command is pipe or no to know which func we will pass it to ypu
+		//if we find | on the command that means pipe
+		if strings.Contains(line, "|") {
+			HandlingPipe(line)
+		} else {
+			HandlingNonPipe(line)
 		}
-
-		//now here we will start new process and this function is built in os package
-		//func StartProcess(name string, argv []string, attr *ProcAttr) (*Process, error)
-
-		//first thing we will need to make an proceattr instance and tell him that this child process can treat with the stdin and std out and so on
-		child := &os.ProcAttr{ //we need to put & cause it is pointer
-			Dir:   "",                                         //this is related to the directory that the child will work on it and i want it to work from the parent directory
-			Env:   nil,                                        //it is the environment variables and i will make it take the same of the parent too
-			Files: []*os.File{os.Stdin, os.Stdout, os.Stderr}, //this is to enable the child to can deal with the input and output from inside it and be careful the order here is very important
-		}
-
-    //it is must to give the ardg the command cause the first argument 0 is the commad name 
-		// Args :=[]string{command}//to make the syntax easy to you you say args is arr of string from command 
-
-     //it will throw an error if the command is not found in the external commands 
-		proc, err := os.StartProcess(Args[0], Args, child) //now i start it
-
-    if err != nil {//that means this command not an external command so we will print the error message
-			fmt.Println(err) //print ln here its role is to print in the shell
-      continue
-		}
-
-    //and this can throw error if there is anything make the child terminate in just bad way 
-		 proc.Wait() //now my curr shell will wait until the child finish cause i am single threaded
-
-	
 
 	}
 
 }
-
-
-
-
-//remember to test clear command in your shell 
